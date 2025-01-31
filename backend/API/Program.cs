86657fba1f05ac71d19b8dd80e0a2d6f763e83c9
MalekNoureddine <noureddinemalek50@gmail.com>
2025-01-31 01:46:23 +0100

    🌍 feat-program-cs(localization): Add request localization middleware & support for multiple languages

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,7 +1,10 @@
 using Core;
 using Core.MiddleWare;
 using Infrastracture;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 using ServiceLayer;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +13,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+#region Localization
+builder.Services.AddControllersWithViews();
+builder.Services.AddLocalization(opt =>
+{
+    opt.ResourcesPath = "";
+});
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    List<CultureInfo> supportedCultures = new List<CultureInfo>
+        {
+            new CultureInfo("ar-EG"),
+            new CultureInfo("en-US"),
+            new CultureInfo("de-DE"),
+            new CultureInfo("fr-FR"),
+            new CultureInfo("en-GB")
+        };
+
+    options.DefaultRequestCulture = new RequestCulture("ar-EG");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
+#endregion
 
 #region Dependency injection
 builder.Services.AddInfrastractureDependencies()
@@ -23,6 +51,13 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+#region Localization middleware
+
+var options = app.Services.GetService<IOptions<RequestLocalizationOptions>>();
+app.UseRequestLocalization(options.Value);
+
+#endregion
 
 app.UseMiddleware<ErrorHandlerMiddleware>();
 
