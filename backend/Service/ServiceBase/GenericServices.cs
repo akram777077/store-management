05b8774b18800fedb95554efa633ef3c222ac892
MalekNoureddine <noureddinemalek50@gmail.com>
2025-01-31 19:37:27 +0100

    Refactor GenericServices and IGenericService
    Updated GenericServices.cs to include Microsoft.EntityFrameworkCore namespace. Removed several methods: AddRangeAsync, GetTableNoTracking, GetTableAsTracking, UpdateRangeAsync, DeleteRangeAsync, Commit, RollBack, SaveChangesAsync, and BeginTransaction. Changed GetByIdAsync parameter type from int to long. Added GetListAsync method. Corresponding changes made in IGenericService interface.

--- backend/Service/ServiceBase/GenericServices.cs
+++ backend/Service/ServiceBase/GenericServices.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastracture.Base;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 using System;
 using System.Collections.Generic;
@@ -25,29 +26,12 @@ namespace ServiceLayer.ServiceBase
             return await _repository.AddAsync(entity);
         }
 
-        // Add multiple entities
-        public async Task AddRangeAsync(ICollection<T> entities)
-        {
-            await _repository.AddRangeAsync(entities);
-        }
-
         // Get by Id
-        public async Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(long id)
         {
             return await _repository.GetByIdAsync(id);
         }
 
-        // Get all entities with no tracking
-        public IQueryable<T> GetTableNoTracking()
-        {
-            return _repository.GetTableNoTracking();
-        }
-
-        // Get all entities with tracking
-        public IQueryable<T> GetTableAsTracking()
-        {
-            return _repository.GetTableAsTracking();
-        }
 
         // Update a single entity
         public async Task UpdateAsync(T entity)
@@ -55,46 +39,16 @@ namespace ServiceLayer.ServiceBase
             await _repository.UpdateAsync(entity);
         }
 
-        // Update multiple entities
-        public async Task UpdateRangeAsync(ICollection<T> entities)
-        {
-            await _repository.UpdateRangeAsync(entities);
-        }
-
+        
         // Delete a single entity
         public async Task DeleteAsync(T entity)
         {
             await _repository.DeleteAsync(entity);
         }
 
-        // Delete multiple entities
-        public async Task DeleteRangeAsync(ICollection<T> entities)
+        public async Task<List<T>> GetListAsync()
         {
-            await _repository.DeleteRangeAsync(entities);
-        }
-
-        // Commit changes
-        public void Commit()
-        {
-            _repository.Commit();
-        }
-
-        // Rollback changes
-        public void RollBack()
-        {
-            _repository.RollBack();
-        }
-
-        // Save changes
-        public async Task SaveChangesAsync()
-        {
-            await _repository.SaveChangesAsync();
-        }
-
-        // Begin a transaction
-        public IDbContextTransaction BeginTransaction()
-        {
-            return _repository.BeginTransaction();
+            return await _repository.GetTableNoTracking().ToListAsync();
         }
 
         #endregion
