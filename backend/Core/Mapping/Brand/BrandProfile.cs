493229558a3046e417a36057995de87810506a4b
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-12 23:02:32 +0100

    feat(core): Add BrandProfile with GetBrandMapping for AutoMapper configuration
    - Created BrandProfile class inheriting from Profile

---
+++ backend/Core/Mapping/Brand/BrandProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mapping.Brands
+{
+    public partial class BrandProfile : Profile
+    {
+        public BrandProfile()
+        {
+            GetBrandMapping();
+        }
+    }
+}
