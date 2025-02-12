68bbe2cd3a90ae51ee49dee884c4b7257408cdda
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-12 22:53:50 +0100

    feat(core): Add GetBrandByIdRequest for brand ID-based queries
    - Added GetBrandByIdRequest class implementing IRequest<Response<GetBrandResponse>>
    - Includes Id property for querying brands by ID

---
+++ backend/Core/Featurs/Brand/Query/Request/GetBrandByIdRequest.cs
@@ -0,0 +1,17 @@
+﻿using Core.Bases;
+using Core.Featurs.Brands.Query.Response;
+using Core.Featurs.UnitTypes.Query.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Brands.Query.Request
+{
+    public class GetBrandByIdRequest(long id) : IRequest<Response<GetBrandResponse>>
+    {
+        public long Id { get; set; } = id;
+    }
+}
