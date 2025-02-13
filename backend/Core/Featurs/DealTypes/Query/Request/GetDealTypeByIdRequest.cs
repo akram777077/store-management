afc56a119adeeeb4b78c84d8da9870d4b742088a
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-13 09:26:55 +0100

    feat(core): Add GetDealTypeByIdRequest for DealType ID-based queries - Added GetDealTypeByIdRequest class implementing IRequest<Response<GetDealTypeResponse>> - Includes Id property for querying DealTypes by ID

---
+++ backend/Core/Featurs/DealTypes/Query/Request/GetDealTypeByIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using Core.Bases;
+using Core.Featurs.DealTypes.Query.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.DealTypes.Query.Request
+{
+    public class GetDealTypeByIdRequest(long id) : IRequest<Response<GetDealTypeResponse>>
+    {
+        public long Id { get; set; } = id;
+    }
+}
