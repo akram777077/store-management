da19e93547dbcb29863bf7757bc896a5ad7d14c4
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-13 10:27:40 +0100

    feat(core): Add GetPaymentMethodByNameRequest for PaymentMethod name-based queries
    - Added GetPaymentMethodByNameRequest class implementing IRequest<Response<GetPaymentMethodResponse>>
    - Includes Name property for querying PaymentMethods by name

---
+++ backend/Core/Featurs/PaymentMethods/Query/Request/GetPaymentMethodByNameRequest.cs
@@ -0,0 +1,11 @@
+﻿using Core.Bases;
+using Core.Featurs.PaymentMethods.Query.Response;
+using MediatR;
+
+namespace Core.Featurs.PaymentMethods.Query.Request
+{
+    public class GetPaymentMethodByNameRequest(string name) : IRequest<Response<GetPaymentMethodResponse>>
+    {
+        public string Name { get; set; } = name;
+    }
+}
