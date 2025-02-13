4f508633bbeaf905828f7c970fa57ba8661b2f2f
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-13 10:30:30 +0100

    feat(core): Add mapping configuration for PaymentMethod to GetPaymentMethodResponse
    - Added GetPaymentMethodMapping method in PaymentMethodProfile class
    - Configured AutoMapper mapping from PaymentMethod to GetPaymentMethodResponse
    - Updated PaymentMethodProfile constructor to call GetPaymentMethodMapping method

---
+++ backend/Core/Mapping/PaymentMethods/Query/GetPaymentMethodMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Core.Featurs.PaymentMethods.Query.Response;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mapping.PaymentMethods
+{
+    public partial class PaymentMethodProfile : Profile
+    {
+        public void GetPaymentMethodMapping()
+        {
+            CreateMap<PaymentMethod, GetPaymentMethodResponse>();
+        }
+    }
+}
