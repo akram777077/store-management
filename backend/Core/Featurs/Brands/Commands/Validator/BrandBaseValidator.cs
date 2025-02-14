5241626b4ac68a6126d716f5f9c85bc3ea247a36
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-14 21:42:40 +0100

    feat(core): Add `BrandBaseValidator` for shared validation rules
    - Added `BrandBaseValidator` to encapsulate shared validation rules for brand commands.
    - Validates that the `Name` property is not empty, not null, and within length constraints (2-30 characters).
    - Uses `IStringLocalizer` for localized error messages.

---
+++ backend/Core/Featurs/Brands/Commands/Validator/BrandBaseValidator.cs
@@ -0,0 +1,18 @@
+﻿using Core.Featurs.Brands.Commands.Requests;
+using Core.Localization;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Core.Featurs.Brands.Commands.Validator
+{
+    public class BrandBaseValidator<T> : AbstractValidator<T> where T : BrandBaseCommand
+    {
+        public BrandBaseValidator(IStringLocalizer<SharedResources> stringLocalizer)
+        {
+            RuleFor(s => s.Name)
+                .NotEmpty().WithMessage(stringLocalizer[SharedResourcesKeys.NotEmpty])
+                .NotNull().WithMessage(stringLocalizer[SharedResourcesKeys.NotNull])
+                .Length(2, 30);
+        }
+    }
+}
