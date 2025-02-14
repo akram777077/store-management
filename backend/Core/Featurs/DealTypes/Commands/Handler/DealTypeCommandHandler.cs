a2dd6991121d1dabd0d54ef920519d3f2c827e68
akram777077 <akram7.dris7@gmail.com>
2025-02-14 14:29:33 +0100

    feat(core): add Handle method for UpdateDealTypeCommand in DealTypeCommandHandler
    - Implement `Handle` method for `UpdateDealTypeCommand` in `DealTypeCommandHandler`
    - Retrieve the deal type by ID using `_dealTypeService.GetByIdAsync`
    - Return `NotFound` response if the deal type does not exist
    - Map the request to the existing deal type entity using AutoMapper
    - Update the deal type using `_dealTypeService.UpdateAsync`
    - Return a `Success` response with an empty message

--- backend/Core/Featurs/DealTypes/Commands/Handler/DealTypeCommandHandler.cs
+++ backend/Core/Featurs/DealTypes/Commands/Handler/DealTypeCommandHandler.cs
@@ -11,12 +11,24 @@ using ServiceLayer.Interfaces;
 namespace Core.Featurs.DealTypes.Commands.Handler;
 
 public class DealTypeCommandHandler(IStringLocalizer<SharedResources> stringLocalizer, IDealTypeService dealTypeService, IMapper mapper) : ResponseHandler(stringLocalizer),
-    IRequestHandler<CreateDealTypeCommand, Response<string>>
+    IRequestHandler<CreateDealTypeCommand, Response<string>>,
+    IRequestHandler<UpdateDealTypeCommand, Response<string>>
 {
     private readonly IStringLocalizer<SharedResources> _stringLocalizer = stringLocalizer;
     private readonly IDealTypeService _dealTypeService = dealTypeService;
     private readonly IMapper _mapper = mapper;
 
+    public async Task<Response<string>> Handle(UpdateDealTypeCommand request, CancellationToken cancellationToken)
+    {
+        var dealType = await _dealTypeService.GetByIdAsync(request.Id);
+        if (dealType == null)
+            return NotFound<string>();
+
+        _mapper.Map(request, dealType);
+
+        await _dealTypeService.UpdateAsync(dealType);
+        return Success("");
+    }
     public async Task<Response<string>> Handle(CreateDealTypeCommand request, CancellationToken cancellationToken)
     {
         var dealType = _mapper.Map<DealType>(request);
