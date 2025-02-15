0d04a769d832c97f550a502a5f7dbcb03f0e23d6
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 11:49:07 +0100

    feat(core): Add `SaleTypeProfile` for AutoMapper configuration
    - Created `SaleTypeProfile` class inheriting from `Profile`

---
+++ backend/Core/Mapping/SaleTypes/SaleTypeProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Core.Featurs.Categories.Commands.Requests;
+using Core.Featurs.Categories.Queries.Responses;
+using Core.Featurs.SaleTypes.Query.Response;
+using Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mapping.SaleTypes
+{
+    public partial class SaleTypeProfile : Profile
+    {
+        public SaleTypeProfile()
+        {
+            GetSaleTypesMapping();
+            CreateSaleTypeMapping();
+            EditSaleTypeMapping();
+        }
+    }
+   
+}
+
+
