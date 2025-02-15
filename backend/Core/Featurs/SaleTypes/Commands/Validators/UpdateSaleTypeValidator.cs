09199ff62ba32a7a4f7cf41d7d0f5fd0e4ae4acc
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 11:58:32 +0100

    feat(core): Add `UpdateSaleTypeValidator`
    - Added `UpdateSaleTypeValidator` to validate `UpdateSaleTypeCommand`.
    - Inherits from `SaleTypeBaseValidator` for shared validation rules.
    - Adds custom validation to ensure the sale type name is unique, excluding the current sale type being updated.

---
+++ backend/Core/Featurs/SaleTypes/Commands/Validators/UpdateSaleTypeValidator.cs
@@ -0,0 +1,45 @@
+﻿using Core.Featurs.SaleTypes.Commands.Requests;
+using Core.Localization;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using ServiceLayer.Interfaces;
+
+namespace Core.Featurs.SaleTypes.Commands.Validators
+{
+    public class UpdateSaleTypeValidator : AbstractValidator<UpdateSaleTypeCommand>
+    {
+        #region Fields
+        private readonly ISaleTypeService _saleTypeService;
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        #endregion
+
+        #region Constructor
+        public UpdateSaleTypeValidator(ISaleTypeService saleTypeService, IStringLocalizer<SharedResources> stringLocalizer)
+        {
+            _saleTypeService = saleTypeService;
+            _stringLocalizer = stringLocalizer;
+            ApplyValidationRules();
+            ApplyCostumeValidationRules();
+        }
+        #endregion
+
+        #region Actions
+        public void ApplyValidationRules()
+        {
+            RuleFor(s => s.Id)
+                .NotEmpty().WithMessage(_stringLocalizer[SharedResourcesKeys.NotEmpty])
+                .GreaterThan(0).WithMessage(_stringLocalizer[SharedResourcesKeys.IdGreaterThanZero]);
+
+            Include(new SaleTypeBaseValidator<UpdateSaleTypeCommand>(_stringLocalizer));
+
+        }
+        public void ApplyCostumeValidationRules()
+        {
+            RuleFor(s => s.Name)
+                .MustAsync(async (module, key, cancellationToken) => !await _saleTypeService.IsSaleTypeNameExists(module.Name, module.Id))
+                .WithMessage(_stringLocalizer[SharedResourcesKeys.IsAlreadyExits]);
+
+        }
+        #endregion
+    }
+}
