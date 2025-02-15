095d9fe4ef4528b629b5b0c416f19dde25f11e13
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 10:27:56 +0100

    feat(core): Add `UpdatePaymentMethodCommand`
    - Added `UpdatePaymentMethodCommand` for updating an existing payment method.
    - Inherits from `PaymentMethodBaseCommand` and includes an `Id` property to identify the payment method to update.

---
+++ backend/Core/Featurs/PaymentMethods/Commands/Requests/UpdatePaymentMethodCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Featurs.PaymentMethods.Commands.Requests
+{
+    public class UpdatePaymentMethodCommand : PaymentMethodBaseCommand
+    {
+        public int Id { get; set; }
+    }
+
+}
