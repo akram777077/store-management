0721aaa22e8f8f8cd142ae40ed3b8f7cc194f9b2
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 12:01:39 +0100

    feat(core): Implement command handlers for `SaleType` operations
    - Added `SaleTypeCommandHandler` to handle `CreateSaleTypeCommand`, `UpdateSaleTypeCommand`, and `DeleteSaleTypeCommand`.
    - Used `ISaleTypeService` to perform CRUD operations on the `SaleType` entity.
    - Mapped command requests to the `SaleType` entity using `IMapper`.
    - Handlers return consistent responses (e.g., `Created`, `NotFound`, `Deleted`) using the `ResponseHandler` base class.

---
+++ backend/Core/Featurs/SaleTypes/Commands/Handlers/SaleTypeCommandHandler.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Core.Bases;
+using Core.Featurs.SaleTypes.Commands.Requests;
+using Core.Localization;
+using Data.Entities;
+using MediatR;
+using Microsoft.Extensions.Localization;
+using ServiceLayer.Interfaces;
+
+namespace Core.Featurs.SaleTypes.Commands.Handlers
+{
+    public class SaleTypeCommandHandler : ResponseHandler,
+       IRequestHandler<CreateSaleTypeCommand, Response<string>>,
+       IRequestHandler<UpdateSaleTypeCommand, Response<string>>,
+       IRequestHandler<DeleteSaleTypeCommand, Response<string>>
+    {
+        #region Properties
+        private readonly IMapper _mapper;
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        private readonly ISaleTypeService _saleTypeService;
+        #endregion
+
+        #region Constructor
+        public SaleTypeCommandHandler(IStringLocalizer<SharedResources> stringLocalizer, ISaleTypeService saleTypeService, IMapper mapper) 
+            : base(stringLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+            _saleTypeService = saleTypeService;
+            _mapper = mapper;
+        }
+        #endregion
+
+        #region Function Handling
+        public async Task<Response<string>> Handle(UpdateSaleTypeCommand request, CancellationToken cancellationToken)
+        {
+            var saleType = await _saleTypeService.GetByIdAsync(request.Id);
+            if (saleType == null)
+                return NotFound<string>();
+
+            _mapper.Map(request, saleType);
+
+            await _saleTypeService.UpdateAsync(saleType);
+            return Success("");
+        }
+
+        public async Task<Response<string>> Handle(CreateSaleTypeCommand request, CancellationToken cancellationToken)
+        {
+            var saleType = _mapper.Map<SaleType>(request);
+            var response = await _saleTypeService.AddAsync(saleType);
+
+            return Created("");
+        }
+
+        public async Task<Response<string>> Handle(DeleteSaleTypeCommand request, CancellationToken cancellationToken)
+        {
+            if (request.Id < 1)
+                return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.IdGreaterThanZero]);
+
+            var saleType = await _saleTypeService.GetByIdAsync(request.Id);
+            if (saleType == null)
+                return NotFound<string>();
+
+            var result = await _saleTypeService.DeleteAsync(saleType);
+
+            if (result == "Deleted") 
+                return Deleted<string>("");
+            
+            return InternalServerError<string>();
+        }
+        #endregion
+    }
+}
