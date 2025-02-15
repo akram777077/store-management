dc7012b28e452ded381d918734367081e48d8369
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 11:57:10 +0100

    feat(core): Add `DeleteSaleTypeCommand`
    - Added `DeleteSaleTypeCommand` for deleting a sale type by ID.
    - Includes an `Id` property to identify the sale type to delete.

---
+++ backend/Core/Featurs/SaleTypes/Commands/Requests/DeleteSaleTypeCommand.cs
@@ -0,0 +1,10 @@
+﻿using Core.Bases;
+using MediatR;
+
+namespace Core.Featurs.SaleTypes.Commands.Requests
+{
+    public class DeleteSaleTypeCommand : IRequest<Response<string>>
+    {
+        public int Id { get; set; }
+    }
+}
