674ce028f2d3c398509025754409830208c1633f
akram777077 <akram7.dris7@gmail.com>
2025-02-15 21:44:58 +0100

    feat(API): Add UpdateBrand endpoint to BrandController
    - Implemented `UpdateBrand` method to update brand details by ID.
    - Defined `[HttpPut]` route using `Router.BrandRoute.Update` for consistency with the data layer.
    - Accepts `id` as a route parameter and `BrandBaseCommand` as a request body.
    - Uses `_mediator` to handle requests via `UpdateBrandCommand`.
    
    This update enhances API functionality by enabling brand updates with a structured approach.

--- backend/API/Controllers/BrandController.cs
+++ backend/API/Controllers/BrandController.cs
@@ -1,4 +1,5 @@
 using API.Base;
+using Core.Featurs.Brands.Commands.Requests;
 using Core.Featurs.Brands.Query.Request;
 using Data.AppMetaData;
 using Data.Entities;
@@ -21,6 +22,18 @@ public class BrandController(IMediator mediator) : AppBaseController(mediator)
     public async Task<ActionResult<Brand>> GetBrandById([FromRoute] int id)
     {
         var response = await _mediator.Send(new GetBrandByIdRequest(id));
+        return NewResult(response);
+    }
+    [HttpPut]
+    [Route(Router.BrandRoute.Update)]
+    public async Task<ActionResult<Brand>> UpdateBrand(int id,[FromBody] BrandBaseCommand request)
+    {
+        var updateRequest = new UpdateBrandCommand
+        {
+            Id = id,
+            Name = request.Name
+        };
+        var response = await _mediator.Send(updateRequest);
         return NewResult(response);
     }
 }
