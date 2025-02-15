d5bc4de8e4e7ecbd44d29e434b089d0f1b7b0060
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 10:32:32 +0100

    feat(core): Add `PaymentMethodBaseValidator`
    - Added `PaymentMethodBaseValidator` to encapsulate shared validation rules for payment method commands.
    - Validates that the `Name` property is not empty, not null, and within length constraints (2-30 characters).
    - Uses `IStringLocalizer` for localized error messages.

---
+++ backend/Core/Featurs/PaymentMethods/Commands/Validators/PaymentMethodBaseValidator.cs
@@ -0,0 +1,24 @@
+﻿using Core.Featurs.PaymentMethods.Commands.Requests;
+using Core.Localization;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using ServiceLayer.ServiceImplementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.PaymentMethods.Commands.Validators
+{
+    public class PaymentMethodBaseValidator<T> : AbstractValidator<T> where T : PaymentMethodBaseCommand
+    {
+        public PaymentMethodBaseValidator(IStringLocalizer<SharedResources> stringLocalizer)
+        {
+            RuleFor(s => s.Name)
+                .NotEmpty().WithMessage(stringLocalizer[SharedResourcesKeys.NotEmpty])
+                .NotNull().WithMessage(stringLocalizer[SharedResourcesKeys.NotNull])
+                .Length(2, 30);
+        }
+    }
+}
