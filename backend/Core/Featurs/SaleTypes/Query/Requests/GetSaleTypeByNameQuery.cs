8b9ec7ca03d10037dd1d1a3566ba0d60cb5aca8b
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 11:46:34 +0100

    feat(core): Add `GetSaleTypeByNameRequest` for `SaleType` name-based queries
    - Added `GetSaleTypeByNameRequest` class implementing `IRequest<Response<GetSaleTypesResponse>>`
    - Includes `Name` property for querying `SaleTypes` by name

---
+++ backend/Core/Featurs/SaleTypes/Query/Requests/GetSaleTypeByNameQuery.cs
@@ -0,0 +1,12 @@
+﻿using Core.Bases;
+using Core.Featurs.Categories.Queries.Responses;
+using Core.Featurs.SaleTypes.Query.Response;
+using MediatR;
+
+namespace Core.Featurs.SaleTypes.Query.Requests
+{
+    public class GetSaleTypeByNameQuery(string name) : IRequest<Response<GetSaleTypesResponse>>
+    {
+        public string Name { get; set; } = name;
+    }
+}
