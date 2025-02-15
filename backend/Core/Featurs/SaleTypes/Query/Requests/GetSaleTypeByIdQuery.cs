e0f14cc2b999ca765977b88e51ed931f96926fdd
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 11:45:55 +0100

    feat(core): Add `GetSaleTypeByIdRequest` for `SaleType` ID-based queries
    - Added `GetSaleTypeByIdRequest` class implementing `IRequest<Response<GetSaleTypesResponse>>`
    - Includes `Id` property for querying `SaleTypes` by ID

---
+++ backend/Core/Featurs/SaleTypes/Query/Requests/GetSaleTypeByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Core.Bases;
+using Core.Featurs.SaleTypes.Query.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.SaleTypes.Query.Requests
+{
+    public class GetSaleTypeByIdQuery(long id) : IRequest<Response<GetSaleTypesResponse>>
+    {
+        public long Id { get; set; } = id;
+    }
+}
