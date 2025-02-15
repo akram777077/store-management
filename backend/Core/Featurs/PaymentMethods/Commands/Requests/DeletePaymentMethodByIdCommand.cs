a642d49bd9b10d2704b417bac31e490b77fd2c54
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 10:27:10 +0100

    feat(core): Add `DeletePaymentMethodCommand`
    - Added `DeletePaymentMethodCommand` for deleting a payment method by ID.
    - Includes an `Id` property to identify the payment method to delete.

---
+++ backend/Core/Featurs/PaymentMethods/Commands/Requests/DeletePaymentMethodByIdCommand.cs
@@ -0,0 +1,11 @@
+﻿using Core.Bases;
+using MediatR;
+
+namespace Core.Featurs.PaymentMethods.Commands.Requests
+{
+    public class DeletePaymentMethodByIdCommand : IRequest<Response<string>>
+    {
+        public int Id { get; set; }
+    }
+
+}
