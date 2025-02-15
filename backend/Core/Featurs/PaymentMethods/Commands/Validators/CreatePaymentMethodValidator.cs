ee56eb98d235d1798e657b387e2c1d7efb17e95c
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 10:32:54 +0100

    feat(core): Add `CreatePaymentMethodValidator`
    - Added `CreatePaymentMethodValidator` to validate `CreatePaymentMethodCommand`.
    - Inherits from `PaymentMethodBaseValidator` for shared validation rules.
    - Adds custom validation to ensure the payment method name is unique.

---
+++ backend/Core/Featurs/PaymentMethods/Commands/Validators/CreatePaymentMethodValidator.cs
@@ -0,0 +1,33 @@
+﻿using Core.Featurs.PaymentMethods.Commands.Requests;
+using Core.Localization;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using ServiceLayer.Interfaces;
+
+namespace Core.Featurs.PaymentMethods.Commands.Validators
+{
+    public class CreatePaymentMethodValidator : AbstractValidator<CreatePaymentMethodCommand>
+    {
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        private readonly IPaymentMethodService _paymentMethodService;
+
+        public CreatePaymentMethodValidator(IStringLocalizer<SharedResources> localizer, IPaymentMethodService paymentMethodService)
+        {
+            _stringLocalizer = localizer;
+            _paymentMethodService = paymentMethodService;
+            ApplyValidationRules();
+            ApplyCostumeValidationRules();
+        }
+        public void ApplyValidationRules()
+        {
+            Include(new PaymentMethodBaseValidator<CreatePaymentMethodCommand>(_stringLocalizer));
+        }
+
+        public void ApplyCostumeValidationRules()
+        {
+            RuleFor(s => s.Name)
+                .MustAsync(async (module, key, cancellationToken) => !await _paymentMethodService.IsPaymentMethodNameExistsAsync(module.Name))
+                .WithMessage(_stringLocalizer[SharedResourcesKeys.IsAlreadyExits]);
+        }
+    }
+}
