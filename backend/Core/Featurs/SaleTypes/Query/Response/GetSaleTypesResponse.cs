ce26897ccec1b5554613cecc85b6cc36fe27a01e
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-02-15 11:48:02 +0100

    feat(core): Add `GetSaleTypesResponse` for `SaleType` query responses - Added `GetSaleTypesResponse` class with properties:   - `Id` (long)   - `Name` (required string)   - `Description` (optional string) - Used as the response DTO for `SaleType`-related queries

---
+++ backend/Core/Featurs/SaleTypes/Query/Response/GetSaleTypesResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.SaleTypes.Query.Response
+{
+    public class GetSaleTypesResponse
+    {
+        public long Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
