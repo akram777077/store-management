a32b6c89c8bb2ee4eed85f666e58270f08df5191
akram777077 <akram7.dris7@gmail.com>
2025-02-17 09:31:54 +0100

    refactor(core): change `Id` type from `int` to `long` in `DeleteSaleTypeCommand`
    - Refactor `DeleteSaleTypeCommand` to use `long` for the `Id` property
    - Ensure consistency with other API layers and route parameter constraints
    
    This change refines the `DeleteSaleTypeCommand` to handle `long` type for `Id`, aligning with the updated routing conventions.

--- backend/Core/Featurs/SaleTypes/Commands/Requests/DeleteSaleTypeCommand.cs
+++ backend/Core/Featurs/SaleTypes/Commands/Requests/DeleteSaleTypeCommand.cs
@@ -5,6 +5,6 @@ namespace Core.Featurs.SaleTypes.Commands.Requests
 {
     public class DeleteSaleTypeCommand : IRequest<Response<string>>
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
     }
 }
