37bec1df5548da1dd66ffa778937713ed8075d8a
akram777077 <akram7.dris7@gmail.com>
2025-02-19 00:52:18 +0100

    feat(data): Add route for retrieving payment method by ID
    - Introduced `GetById` constant in `PaymentMethodRouting`.
    - Defines route as `PaymentMethods/id={id}` for fetching payment methods by ID.
    - Enhances API routing consistency and maintainability.
    
    This update improves the data layer by standardizing route definitions.

--- backend/Data/AppMetaData/EntitiesRouting/PaymentMethodRouting.cs
+++ backend/Data/AppMetaData/EntitiesRouting/PaymentMethodRouting.cs
@@ -7,5 +7,6 @@ public partial class Router
         public const string Prefix = "PaymentMethods/";
         public const string List = Prefix + "List";
         public const string GetByName = Prefix + "name/{name}";
+        public const string GetById = Prefix + "id={id}";
     }
 }
