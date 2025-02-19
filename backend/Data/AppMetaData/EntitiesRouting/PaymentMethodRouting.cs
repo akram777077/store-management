78ea4f055c2ac02db4414f5d58641e5b939b4b60
akram777077 <akram7.dris7@gmail.com>
2025-02-19 01:20:03 +0100

    feat(data): Add route for updating payment methods
    - Introduced `Update` constant in `PaymentMethodRouting`.
    - Defines route as `PaymentMethods/Update/{id}` for updating payment methods.
    - Standardizes API routing and improves maintainability.
    
    This update enhances the data layer by defining a structured update endpoint.

--- backend/Data/AppMetaData/EntitiesRouting/PaymentMethodRouting.cs
+++ backend/Data/AppMetaData/EntitiesRouting/PaymentMethodRouting.cs
@@ -9,5 +9,6 @@ public partial class Router
         public const string GetByName = Prefix + "name/{name}";
         public const string GetById = Prefix + "id={id}";
         public const string Add = Prefix + "Add";
+        public const string Update = Prefix + "Update/{id}";
     }
 }
