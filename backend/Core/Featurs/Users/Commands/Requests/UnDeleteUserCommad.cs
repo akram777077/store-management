a6cefb77ee7e1fbca835d576b93b8ba11003fed1
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:56:59 +0100

    Add UnDeleteUserCommand Class to Handle User Undeletion
    - Introduced UnDeleteUserCommand in the Core.Features.Users.Commands.Requests namespace.
    - Implements IRequest<Response<string>> from MediatR, indicating an expected Response<string>.
    - Includes a required Id property of type string to identify the user to be undeleted.
    - Added necessary using directives for Core.Bases, MediatR, System, System.Collections.Generic, System.Linq, System.Text, and System.Threading.Tasks.

---
+++ backend/Core/Featurs/Users/Commands/Requests/UnDeleteUserCommad.cs
@@ -0,0 +1,16 @@
+﻿using Core.Bases;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Commands.Requests
+{
+    public class UnDeleteUserCommad : IRequest<Response<string>>
+    {
+        public required string Id { get; set; }
+
+    }
+}
