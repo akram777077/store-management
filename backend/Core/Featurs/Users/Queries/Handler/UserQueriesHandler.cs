101f766f58516283fe0b17ba55ef038e0d66d228
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:05:19 +0100

    Add UserQueriesHandler for Handling Various User Queries
    - Introduced UserQueriesHandler in the Core.Features.Users.Queries.Handler namespace.
    - Handles user-related queries via MediatR, including:
      • GetUserByIdQuery
      • GetUsersListQuery with pagination
      • GetUserByUsernameQuery
      • GetUserByPhoneNumberQuery
      • GetUserByEmailQuery
    - Inherits from ResponseHandler and implements relevant IRequestHandler interfaces.
    - Utilizes dependency injection for IStringLocalizer, IMapper, and UserManager<User>.
    - Each Handle method retrieves user data and includes error handling for "Not Found" responses.

---
+++ backend/Core/Featurs/Users/Queries/Handler/UserQueriesHandler.cs
@@ -0,0 +1,90 @@
+﻿using AutoMapper;
+using Core.Bases;
+using Core.Featurs.Users.Queries.Request;
+using Core.Featurs.Users.Queries.Response;
+using Core.Localization;
+using Core.Wrapper;
+using Data.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Queries.Handler
+{
+    public class UserQueriesHandler : ResponseHandler,
+        IRequestHandler<GetUserByIdQuery, Response<GetUserResponse>>,
+        IRequestHandler<GetUsersListQuery, PaginatedResult<GetUserResponse>>,
+        IRequestHandler<GetUserByUsernameQuery, Response<GetUserResponse>>,
+        IRequestHandler<GetUserByPhoneNumberQuery, Response<GetUserResponse>>,
+        IRequestHandler<GetUserByEmailQuery, Response<GetUserResponse>>
+
+    {
+        #region Fields
+        private readonly IMapper _mapper;
+        private readonly IStringLocalizer<SharedResources> _sharedResources;
+        private readonly UserManager<User> _userManager;
+        #endregion
+
+        #region Constructors
+        public UserQueriesHandler(IStringLocalizer<SharedResources> stringLocalizer,
+                                  IMapper mapper,
+                                  UserManager<User> userManager) : base(stringLocalizer)
+        {
+            _mapper = mapper;
+            _sharedResources = stringLocalizer;
+            _userManager = userManager;
+        }
+        #endregion
+
+        #region Handle Functions
+        public async Task<PaginatedResult<GetUserResponse>> Handle(GetUsersListQuery request, CancellationToken cancellationToken)
+        {
+            var users = _userManager.Users.AsQueryable();
+            var paginatedList = await _mapper.ProjectTo<GetUserResponse>(users)
+                                            .ToPaginatedListAsync(request.PageNumber, request.PageNumber);
+            return paginatedList;
+        }
+        
+        public async Task<Response<GetUserResponse>> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(request.Id);
+            if (user == null) return NotFound<GetUserResponse>(_sharedResources[SharedResourcesKeys.NotFound]);
+            var result = _mapper.Map<GetUserResponse>(user);
+            return Success(result);
+        }
+
+        public async Task<Response<GetUserResponse>> Handle(GetUserByUsernameQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByNameAsync(request.Username);
+            if (user == null) return NotFound<GetUserResponse>(_sharedResources[SharedResourcesKeys.NotFound]);
+            var result = _mapper.Map<GetUserResponse>(user);
+            return Success(result);
+        }
+
+        public async Task<Response<GetUserResponse>> Handle(GetUserByPhoneNumberQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.Users
+                                         .AsNoTracking()
+                                         .FirstOrDefaultAsync(x => x.PhoneNumber == request.PhoneNumber, cancellationToken);
+            if (user == null) return NotFound<GetUserResponse>(_sharedResources[SharedResourcesKeys.NotFound]);
+            var result = _mapper.Map<GetUserResponse>(user);
+            return Success(result);
+        }
+
+        public async Task<Response<GetUserResponse>> Handle(GetUserByEmailQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.Email);
+            if (user == null) return NotFound<GetUserResponse>(_sharedResources[SharedResourcesKeys.NotFound]);
+            var result = _mapper.Map<GetUserResponse>(user);
+            return Success(result);
+        }
+        #endregion
+
+    }
+}
