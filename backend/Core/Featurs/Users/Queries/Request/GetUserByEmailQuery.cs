1babe4ff2701de8d28f3fc9c7e20e1c9e9fff3b7
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:03:50 +0100

    Add new user query classes for email, phone, and username
    - Introduced GetUserByEmailQuery, GetUserByPhoneNumberQuery, and GetUserByUsernameQuery in the Core.Features.Users.Queries.Request namespace.
    - Each query class includes a specific property:
      • GetUserByEmailQuery includes an Email property.
      • GetUserByPhoneNumberQuery includes a PhoneNumber property.
      • GetUserByUsernameQuery includes a Username property.
    - All classes implement IRequest<Response<GetUserResponse>> from MediatR, indicating they expect a Response<GetUserResponse>.

---
+++ backend/Core/Featurs/Users/Queries/Request/GetUserByEmailQuery.cs
@@ -0,0 +1,17 @@
+﻿using Core.Bases;
+using Core.Featurs.Users.Queries.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Queries.Request
+{
+    public class GetUserByEmailQuery : IRequest<Response<GetUserResponse>>
+    {
+        public string Email { get; set; }
+    }
+}
+
---
+++ backend/Core/Featurs/Users/Queries/Request/GetUserByUsernameQuery.cs
@@ -0,0 +1,16 @@
+﻿using Core.Bases;
+using Core.Featurs.Users.Queries.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Queries.Request
+{
+    public class GetUserByUsernameQuery : IRequest<Response<GetUserResponse>>
+    {
+        public string Username { get; set; }
+    }
+}
