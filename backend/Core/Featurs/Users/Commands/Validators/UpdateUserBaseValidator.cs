5be6e5e792e847a30d97a4cd936c0ccdd4b07ba0
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:26:50 +0100

    Add UpdateUser Commands with New Namespace and Using Directives
    - Introduced new using directives for various namespaces (e.g., Core.Bases, MediatR, System).
    - Created a new namespace: Core.Featurs.Users.Commands.Requests.
    - Defined two new classes:
      • UpdateUserBaseCommand: Implements IRequest<Response<string>>, includes properties for UserName, Email, and an optional PhoneNumber.
      • UpdateUserCommand: Inherits from UpdateUserBaseCommand and adds a required Id property.

---
+++ backend/Core/Featurs/Users/Commands/Validators/UpdateUserBaseValidator.cs
@@ -0,0 +1,28 @@
+﻿using Core.Featurs.DealTypes.Commands.Requests;
+using Core.Featurs.Users.Commands.Requests;
+using Core.Localization;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Commands.Validators
+{
+    public class UpdateUserBaseValidator<T> : AbstractValidator<T> where T : UpdateUserBaseCommand
+    {
+        public UpdateUserBaseValidator(IStringLocalizer<SharedResources> stringLocalizer)
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().WithMessage(stringLocalizer[SharedResourcesKeys.NotEmpty])
+                .NotNull().WithMessage(stringLocalizer[SharedResourcesKeys.NotNull])
+                .MaximumLength(100).WithMessage(stringLocalizer[SharedResourcesKeys.MaxLengthis100]);
+
+            RuleFor(x => x.Email)
+                 .NotEmpty().WithMessage(stringLocalizer[SharedResourcesKeys.NotEmpty])
+                 .NotNull().WithMessage(stringLocalizer[SharedResourcesKeys.Required]);
+        }
+    }
+}
---
+++ backend/Core/Featurs/Users/Commands/Validators/UpdateUserValidator.cs
@@ -0,0 +1,59 @@
+﻿using Core.Featurs.DealTypes.Commands.Requests;
+using Core.Featurs.DealTypes.Commands.validator;
+using Core.Featurs.Users.Commands.Requests;
+using Core.Localization;
+using Data.Entities.Identity;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Commands.Validators
+{
+    public class UpdateUserValidator : AbstractValidator<UpdateUserCommand>
+    {
+        #region Fields
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        private readonly UserManager<User> _userManager;
+
+        #endregion
+
+        #region Constructors
+        public UpdateUserValidator(IStringLocalizer<SharedResources> localizer, UserManager<User> userManager)
+        {
+            _stringLocalizer = localizer;
+            _userManager = userManager;
+            ApplyValidationsRules();
+            ApplayCostumeValidationRules();
+        }
+        #endregion
+
+        #region Handle Functions
+        public void ApplyValidationsRules()
+        {
+            RuleFor(s => s.Id).NotEmpty().WithMessage(_stringLocalizer[SharedResourcesKeys.NotEmpty])
+            .NotNull().WithMessage(_stringLocalizer[SharedResourcesKeys.NotNull]);
+            Include(new UpdateUserBaseValidator<UpdateUserCommand>(_stringLocalizer));
+        }
+        public void ApplayCostumeValidationRules()
+        {
+
+            RuleFor(s => s.UserName).MustAsync(async (module, key, cancellationToken) => await _userManager.Users.FirstOrDefaultAsync(x => x.UserName == module.UserName && x.Id != module.Id) == null)
+                .WithMessage(_stringLocalizer[SharedResourcesKeys.IsAlreadyExits]);
+
+            RuleFor(s => s.Email).MustAsync(async (module, key, cancellationToken) => await _userManager.Users.FirstOrDefaultAsync(x => x.Email == module.Email && x.Id != module.Id) == null)
+                .WithMessage(_stringLocalizer[SharedResourcesKeys.IsAlreadyExits]);
+
+            RuleFor(s => s.PhoneNumber).MustAsync(async (module, key, cancellationToken) => !await _userManager.Users.AnyAsync(x => x.PhoneNumber == module.PhoneNumber && x.Id != module.Id))
+                .WithMessage(_stringLocalizer[SharedResourcesKeys.IsAlreadyExits]);
+
+        }
+
+        #endregion
+    }
+}
