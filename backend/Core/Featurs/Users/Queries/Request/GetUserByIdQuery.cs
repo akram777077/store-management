bafc872c9b49ddfba5964d176950a9d7b2d5a65d
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:02:44 +0100

    Add new user query classes for ID and list retrieval
    - Added GetUserByIdQuery in Core.Featurs.Users.Queries.Request:
      • Implements IRequest from MediatR for retrieving a user by ID.
      • Contains a single property, Id (string).
    
    - Added GetUsersListQuery in Core.Featurs.Users.Queries.Request:
      • Implements IRequest from MediatR for retrieving a paginated list of users.
      • Contains properties PageNumber and PageSize (integers).

---
+++ backend/Core/Featurs/Users/Queries/Request/GetUserByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Core.Bases;
+using Core.Featurs.Users.Queries.Response;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Queries.Request
+{
+    public class GetUserByIdQuery : IRequest<Response<GetUserResponse>>
+    {
+        public string Id { get; set; }
+    }
+}
