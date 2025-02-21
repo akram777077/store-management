43f0e9e6383810ef59802166615f4eb5775e9636
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:48:05 +0100

    Add SoftDeleteInterceptor for Soft Delete Functionality
    - Introduced SoftDeleteInterceptor class inheriting from SaveChangesInterceptor.
    - Overrode SavingChangesAsync to handle soft deletion for ISoftDeletable entities.
    - Updated entity state and properties to reflect soft delete behavior.
    - Added necessary using directives and created the new namespace Infrastructure.Interceptors.

---
+++ backend/Infrastracture/Interseptors/SoftDeleteInterseptor.cs
@@ -0,0 +1,43 @@
+﻿using Data.Entities.Contracts;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastracture.Interseptors
+{
+    public sealed class SoftDeleteInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+            DbContextEventData eventData,
+            InterceptionResult<int> result,
+            CancellationToken cancellationToken = default)
+        {
+            if (eventData.Context is null)
+            {
+                return base.SavingChangesAsync(
+                    eventData, result, cancellationToken);
+            }
+
+            IEnumerable<EntityEntry<ISoftDeletable>> entries =
+                eventData
+                    .Context
+                    .ChangeTracker
+                    .Entries<ISoftDeletable>()
+                    .Where(e => e.State == EntityState.Deleted);
+
+            foreach (EntityEntry<ISoftDeletable> softDeletable in entries)
+            {
+                softDeletable.State = EntityState.Modified;
+                softDeletable.Entity.IsDeleted = true;
+                softDeletable.Entity.DeletedOn = DateTime.UtcNow;
+            }
+
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
