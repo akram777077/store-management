a6adafe05ea586f53ad2b7bc176a3b25ad07b1c9
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:35:34 +0100

    Add validators for password reset functionality
    - Introduced ResetPasswordBaseValidator to validate CurrentPassword, NewPassword, and ConfirmPassword fields with localized error messages.
    - Added ResetPasswordValidator that integrates rules from ResetPasswordBaseValidator and includes additional validation for the Id field.

---
+++ backend/Core/Featurs/Users/Commands/Validators/ResetPasswordBaseValidator.cs
@@ -0,0 +1,24 @@
+﻿using Core.Featurs.Users.Commands.Requests;
+using Core.Localization;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Core.Featurs.Users.Commands.Validators
+{
+    public class ResetPasswordBaseValidator<T> : AbstractValidator<T> where T : ResetPasswordBaseCommand
+    {
+        public ResetPasswordBaseValidator(IStringLocalizer<SharedResources> stringLocalizer)
+        {
+            RuleFor(x => x.CurrentPassword)
+                .NotEmpty().WithMessage(stringLocalizer[SharedResourcesKeys.NotEmpty])
+                .NotNull().WithMessage(stringLocalizer[SharedResourcesKeys.Required]);
+
+            RuleFor(x => x.NewPassword)
+                 .NotEmpty().WithMessage(stringLocalizer[SharedResourcesKeys.NotEmpty])
+                 .NotNull().WithMessage(stringLocalizer[SharedResourcesKeys.Required]);
+            RuleFor(x => x.ConfirmPassword)
+                 .Equal(x => x.NewPassword).WithMessage(stringLocalizer[SharedResourcesKeys.PasswordNotEqualConfirmPass]);
+
+        }
+    }
+}
