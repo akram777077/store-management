87d0aff33a6e2ca1ea463f897d2272a3f51a56b5
MalekNoureddine <noureddinemalek50@gmail.com>
2025-02-21 20:34:25 +0100

    Add ResetPassword Commands with Necessary Using Directives
    - Introduced new using directives for Core.Bases, MediatR, System, System.Collections.Generic, System.Linq, System.Text, and System.Threading.Tasks.
    - Created a new namespace: Core.Featurs.Users.Commands.Requests.
    - Defined ResetPasswordBaseCommand class implementing IRequest<Response<string>> with properties: CurrentPassword, NewPassword, and ConfirmPassword.
    - Added ResetPasswordCommand class inheriting from ResetPasswordBaseCommand with an additional Id property.

---
+++ backend/Core/Featurs/Users/Commands/Requests/ResetPasswordCommand.cs
@@ -0,0 +1,21 @@
+﻿using Core.Bases;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Featurs.Users.Commands.Requests
+{
+    public class ResetPasswordBaseCommand : IRequest<Response<string>>
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+    public class ResetPasswordCommand : ResetPasswordBaseCommand
+    {
+        public string Id { get; set; }
+    }
+}
